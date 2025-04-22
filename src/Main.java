f1067559ff4b65f37349fe9efa351055f9410999
Nelli <nelli@mac.mynetworksettings.com>
2025-04-21 20:36:50 -0500

    I did some change

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,18 @@
 public class Main {
     public static void main(String[] args) {
 
+
         int one = 5;
         int two = 4;
         int t3 = 7;
 
         System.out.println(one + two + t3 + "Sum of two numbers");
+
+
+        for (int i = 0; i < 0; i++) {
+            System.out.println("nested loop :);
+        }
+        System.out.println("Hi I little cgange it");
 
     }
 
