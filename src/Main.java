e44ada6fefc8a9a3217abd9197fa222e4f76fd95
Aida Mirpazylova <aida@Aidas-MBP.hsd1.il.comcast.net>
2025-04-21 20:43:40 -0500

    finish

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,6 @@ public class Main {
         int two = 4;
         int t3 = 7;
 
-        System.out.println(one + two + t3 + "Sum of two numbers");
-
+        System.out.println(one + two + "Sum of two numbers" + "!");
     }
 }
